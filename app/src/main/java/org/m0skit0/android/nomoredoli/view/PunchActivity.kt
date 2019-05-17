5fb9a4d39bdec6981a208c53355dbb973593cc40
m0skit0 <ametric@gmail.com>
2019-05-17 18:37:25 +0200

    Loading content view in the punch activity

--- app/src/main/java/org/m0skit0/android/nomoredoli/view/PunchActivity.kt
+++ app/src/main/java/org/m0skit0/android/nomoredoli/view/PunchActivity.kt
@@ -1,10 +1,21 @@
 package org.m0skit0.android.nomoredoli.view
 
+import android.databinding.DataBindingUtil
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import org.koin.android.viewmodel.ext.android.viewModel
+import org.m0skit0.android.nomoredoli.R
+import org.m0skit0.android.nomoredoli.databinding.ActivityPunchBinding
 import org.m0skit0.android.nomoredoli.viewmodel.PunchViewModel
 
 internal class PunchActivity : AppCompatActivity() {
 
     private val punchViewModel: PunchViewModel by viewModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        DataBindingUtil.setContentView<ActivityPunchBinding>(this, R.layout.activity_punch).run {
+            viewmodel = punchViewModel
+        }
+    }
 }
