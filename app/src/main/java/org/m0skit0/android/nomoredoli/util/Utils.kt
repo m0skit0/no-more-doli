130e642098a4738158b87bdd073533d8ab33735c
m0skit0 <ametric@gmail.com>
2019-05-17 22:19:50 +0200

    Added Throwable logging

--- app/src/main/java/org/m0skit0/android/nomoredoli/util/Utils.kt
+++ app/src/main/java/org/m0skit0/android/nomoredoli/util/Utils.kt
@@ -2,9 +2,16 @@ package org.m0skit0.android.nomoredoli.util
 
 import android.app.Activity
 import android.support.annotation.StringRes
+import android.util.Log
 import android.widget.Toast
+
+private const val TAG = "Log"
 
 internal fun Activity.toast(@StringRes id: Int?) {
     Toast.makeText(this, id!!, Toast.LENGTH_SHORT).show()
+}
+
+internal fun Throwable.log() {
+    Log.e(TAG, Log.getStackTraceString(this))
 }
 
